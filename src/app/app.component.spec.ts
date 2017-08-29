259a5bc74853f83e794f75f760cadaf7814b808b
dreamyguy <i@dreamyguy.com>
2017-08-29 18:01:51 +0200

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed, async } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { Pipe, PipeTransform, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ArtistPipe } from './artist.pipe';
+import { SafePipe } from './app.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        FormsModule
+      ],
       declarations: [
-        AppComponent
+        AppComponent,
+        SafePipe,
+        ArtistPipe
       ],
     }).compileComponents();
   }));
@@ -17,16 +26,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'Progressive Rock - Norway'`, async(() => {
+  it(`should have as title 'Progressive Rock Norway'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Progressive Rock - Norway');
+    expect(app.title).toEqual('Progressive Rock Norway');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render the application placeholder', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Progressive Rock - Norway');
+    expect(compiled.querySelector('div').className).toContain('content');
   }));
 });
